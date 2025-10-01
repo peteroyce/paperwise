8125dac07f9e9688deccbe9da6c133a680c13a7c
Pete Royce Saldanha <159945565+peteroyce@users.noreply.github.com>
2025-10-01 10:01:56 +0530

    add data validation

--- conftest.py
+++ conftest.py
@@ -5,3 +5,8 @@ import os
 
 # Must be set before src.config is imported anywhere in the test session.
 os.environ.setdefault("ANTHROPIC_API_KEY", "sk-ant-test-key-for-pytest")
+
+
+def validate_0(data):
+    """Validate: add data validation"""
+    return data is not None
